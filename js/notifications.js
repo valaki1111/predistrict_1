655ca64309cc1de334333de26343ba85ab56637c
valaki1111 <predistrictinfo@gmail.com>
2025-07-19 11:18:25 +0200

    Add files via upload

---
+++ js/notifications.js
@@ -0,0 +1,6 @@
+function sendNotificationToAdmin(message) {
+  // Ide jöhet a tényleges értesítés (pl. email, Telegram, Slack)
+  console.log('Admin notification:', message);
+}
+
+module.exports = { sendNotificationToAdmin };
